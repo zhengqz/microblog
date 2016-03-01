4c20612322fb796bd4b835a6d693f9ccb6f4cb69
zhengqz89 <qinzhou_zheng@163.com>
2016-03-01 22:32:51 +0800

    user login

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,19 @@
 * Create by shylock on 2016/2/29
 """
 
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 from app import views, models
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
